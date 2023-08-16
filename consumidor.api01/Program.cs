f25539e0eac0afc8a29295d3f0dfe29b2859fbe1
felix fabri <felix.fabrijr@gmail.com>
2023-08-16 05:17:37 +0100

    1608

---
+++ consumidor.api01/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using consumidor.api01;
+
+using Flurl;
+using Flurl.Http;
+
+Console.WriteLine("Hello, World!");
+
+string url = "https://localhost:7207/";
+
+item tarefa1 = new item();
+tarefa1.Id = 1;
+tarefa1.Nome = "Pagar conta";
+tarefa1.Finalizado = true;
+
+item tarefa2 = new item();
+tarefa2.Id = 1;
+tarefa2.Nome = "Receber conta xx";
+tarefa2.Finalizado = false;
+
+//gerar uma tarefa
+string endpoint = url + "api/TarefasItems";
+
+//retardar chamada e esperar api estar no ar
+Thread.Sleep(new TimeSpan(0, 0, 5));
+//flurl
+endpoint.PostJsonAsync(tarefa1);
+endpoint.PostJsonAsync(tarefa2);
+
+//ler a lista
+
+//alterar
+
+//ler a lista
+
+//deletar item lista
+
+//ler a lista
+
+Console.WriteLine("Aperte qualquer teclar Finalizar Aplicacao");
+Console.Read();
+
